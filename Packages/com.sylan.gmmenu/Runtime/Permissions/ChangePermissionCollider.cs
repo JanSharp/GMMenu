6856bc1afa46e4d189b7f47d128af70a9078a0b1
SylanTroh <lauranemeth11@gmail.com>
2025-11-23 13:39:48 -0500

    Add ChangePermissionCollider functionality for permission management
    Adds a new script and asset to manage temporary player permissions via collider interaction.

---
+++ Packages/com.sylan.gmmenu/Runtime/Permissions/ChangePermissionCollider.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Sylan.GMMenu
+{
+    public enum PermissionType
+    {
+        DEACTIVATED = PlayerPermissions.PERMISSION_DEACTIVATED,
+        PLAYER = PlayerPermissions.PERMISSION_PLAYER,
+        FACILITATOR = PlayerPermissions.PERMISSION_FACILITATOR,
+        GM = PlayerPermissions.PERMISSION_GM
+    }
+    public class ChangePermissionCollider : GMMenuPart
+    {
+        public PermissionType permissionType;
+        public override void OnPlayerCollisionEnter()
+        {
+            gmMenu.PlayerPermissions.SetTempPermission(permissionType);
+        }
+    }
+}
